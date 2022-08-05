fc010d415896763c5f84d2a7d177a75db18ed561
Grace-pauly <Gpauly2643@conestogac.on.ca>
2022-08-05 17:17:10 -0400

    added scripts

---
+++ DB/data.sql
@@ -0,0 +1,19 @@
+USE LIBRARY
+GO
+
+INSERT INTO Author VALUES('Jim');
+INSERT INTO Author VALUES('Jack');
+INSERT INTO Author VALUES('Tom');
+INSERT INTO Author VALUES('Jerry');
+INSERT INTO Author VALUES('Lin');
+
+GO
+
+INSERT INTO BOOKS VALUES('Harry Potter', 1, 5,5);
+INSERT INTO BOOKS VALUES('Lord of the Rings', 2, 3,3);
+INSERT INTO BOOKS VALUES('Tom & Jerry', 3, 2,2);
+INSERT INTO BOOKS VALUES('Two States', 4, 1,1);
+INSERT INTO BOOKS VALUES('Hide & seek', 5, 4,4);
+
+GO
+INSERT INTO [User] Values('Admin','admin@admin.com','admin');
